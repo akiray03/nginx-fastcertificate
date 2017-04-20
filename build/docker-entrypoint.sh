d76c6ae5436f09ca393c59e5cf2f16b285cb6bea
Akira Yumiyama <yumiyama.akira@gmail.com>
2017-04-21 00:35:38 +0900

    update

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [ -f /usr/local/nginx/conf/nginx.conf ]; then
-  mv /usr/local/nginx/conf/nginx.conf /tmp/nginx.conf
-  for e in $(env | grep -E "_PROXY_TO_(HOST|PORT)" | cut -d'=' -f1)
-  do
-    echo "env $e; # REPLACE_LINE"
-  done > /tmp/env-vars
+rm -f /usr/local/nginx/conf/env_vars.conf
+for e in $(env | grep -E "_PROXY_TO_(HOST|PORT)" | cut -d'=' -f1)
+do
+  echo "env $e; # REPLACE_LINE"
+done > /tmp/env-vars
+echo "worker_processes $NGINX_WORKER_PROCESSES; # REPLACE_LINE" > /usr/local/nginx/conf/env_vars.conf
 
-  echo "worker_processes $NGINX_WORKER_PROCESSES; # REPLACE_LINE" > /tmp/ngx_worker
-  cat /tmp/ngx_worker /tmp/env-vars /tmp/nginx.conf | grep -v REPLACE_LINE > /usr/local/nginx/conf/nginx.conf
+if [ ! -z "$NGINX_WORKER_PROCESSES" ]; then
+  rm -f /usr/local/nginx/conf/worker_processes.conf
+  echo "worker_processes $NGINX_WORKER_PROCESSES;" > /usr/local/nginx/conf/worker_processes.conf
 fi
 
-export INTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
-
-cat /usr/local/nginx/conf/nginx.conf
-
+if [ -z "$INTERNAL_IP" ]; then
+  export INTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+  cat /usr/local/nginx/conf/nginx.conf
+fi
 echo "INTERNAL_IP is $INTERNAL_IP"
 
 exec "$@"
