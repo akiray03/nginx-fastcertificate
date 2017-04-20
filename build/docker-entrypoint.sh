d5e75fc3227b2aba622066050b520779e1a2fd60
Akira Yumiyama <yumiyama.akira@gmail.com>
2017-04-21 00:20:09 +0900

    fixes dupulicate worker_processes entry

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -4,11 +4,11 @@ if [ -f /usr/local/nginx/conf/nginx.conf ]; then
   mv /usr/local/nginx/conf/nginx.conf /tmp/nginx.conf
   for e in $(env | grep -E "_PROXY_TO_(HOST|PORT)" | cut -d'=' -f1)
   do
-    echo "env $e;"
+    echo "env $e; # REPLACE_LINE"
   done > /tmp/env-vars
 
-  echo "worker_processes $NGINX_WORKER_PROCESSES;" > /tmp/ngx_worker
-  cat /tmp/ngx_worker /tmp/env-vars /tmp/nginx.conf | grep -v NGINX_WORKER_PROCESSES > /usr/local/nginx/conf/nginx.conf
+  echo "worker_processes $NGINX_WORKER_PROCESSES; # REPLACE_LINE" > /tmp/ngx_worker
+  cat /tmp/ngx_worker /tmp/env-vars /tmp/nginx.conf | grep -v REPLACE_LINE > /usr/local/nginx/conf/nginx.conf
 fi
 
 export INTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
